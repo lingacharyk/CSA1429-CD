76bd26a11b1c4850b9cd928073c87a912a419b2a
lingacharyk <113505257+lingacharyk@users.noreply.github.com>
2023-05-06 12:06:32 +0530

    Add files via upload

---
+++ p26.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+intmain()
+{
+int a,b,c; 
+/*varible declaration*/printf(“enter two numbers”);
+scanf(“%d %d”,&a,&b);c=a+b;
+//adding two numbersprintf(“sumis %d”,c);
+return0;
+}
